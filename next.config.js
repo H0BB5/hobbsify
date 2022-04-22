dd4c4404040aefa3123d8d591868643973df8996
h0bb5 <dylan.hobbs@hotmail.com>
2022-04-22 05:25:20 -0400

    modify next config to ignore eslint build errors

--- next.config.js
+++ next.config.js
@@ -9,6 +9,11 @@ module.exports = {
     // your project has type errors.
     // !! WARN !!
     ignoreBuildErrors: true
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true
   }
 }
 
